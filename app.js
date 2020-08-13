351e02f7d648d41931f679db6a4071026a802e55
Syahir <888syahir@gmail.com>
2020-08-14 03:34:59 +0800

    add ejs & update routes to app.render

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const express = require('express');
 //express app
 const app = express();
 
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
 	// res.send('<p>Hello :D</p>');
-	res.sendFile('./views/index.html', { root: __dirname });
+	res.render('index');
 });
 
 app.get('/about', (req, res) => {
 	// res.send('<p>Hello :D</p>');
-	res.sendFile('./views/about.html', { root: __dirname });
+	res.render('about');
 });
 
 //redirect to about page
@@ -23,5 +26,5 @@ app.get('/about-us', (req, res) => {
 
 //404
 app.use((req, res) => {
-	res.status(404).sendFile('./views/404.html', { root: __dirname });
+	res.status(404).render('404');
 });
