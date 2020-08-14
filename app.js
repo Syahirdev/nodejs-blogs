fc5ddde77d7b2af1a351ebe8228f23fd907c3df3
Syahir <888syahir@gmail.com>
2020-08-14 19:32:04 +0800

    add mongoDB connection

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
 //express app
 const app = express();
 
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
 
-//listen for requests
-app.listen(3000);
+//middlewares
+const dbURI = 'mongodb+srv://admin:adminonly@cluster0.rmwim.mongodb.net/blogs_db?retryWrites=true&w=majority';
+mongoose
+	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => {
+		//listen for requests
+		app.listen(3000);
+		console.log('Connected to DB');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 app.use(express.static('public'));
 
 app.use(morgan('dev'));
-// app.use((req, res, next) => {
-// 	console.log('1. host: ', req.hostname);
-// 	console.log('2. path: ', req.path);
-// 	console.log('3. method: ', req.method);
-// 	console.log('');
-// 	next();
-// });
 
+//routes
 app.get('/', (req, res) => {
 	const blogs = [
 		{ title: 'Blog Syahir', snippet: 'This is the snippet title for blog Syahir' },
